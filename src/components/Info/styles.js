2bf3d94c9804e6cae3b1be5bf93c058442ef0183
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-10-30 10:39:16 -0300

    Atualizando

--- src/components/Info/styles.js
+++ src/components/Info/styles.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 140rem;
   margin: 2rem auto;
-display: flex;
-justify-content: center;
-align-items: center;
-color: white;
-   img{
-      height: 500px;
-   }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  img {
+    height: 500px;
+  }
 `;
